89d05bd59cc7aa0f27d8c9160bdb2673d30b1171
Gaston <gastonsalimbene@gmail.com>
2021-03-18 16:23:58 +0100

    approach game screen done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,17 @@
 class Game {
-    constructor
+    constructor(canvas, ctx, width, height, callback) {
+        this.canvas= canvas;
+        this.ctx= ctx;
+        this.width= width;
+        this.heigh= height; 
+        this.callback= callback;
+
+        this.player= new Player(canvas, ctx, width/2, height/2)
+    }
+
+    load() {
+        this.canvas.style.backgroundImage= url("/img/Moes_Bar.jpeg");
+    }
+
+    
 }
--- js/main.js
+++ js/main.js
@@ -2,20 +2,25 @@ document.addEventListener('DOMContentLoaded', () =>{
     
     const main_content_element = document.getElementById('content');
 
-    let name,
-        game,
+    let game,
 
         start_button,
         restart_button,
 
         splash_element,
         canvas_element,
-        end_game_element;
+        
 
-    window.create_html= html => {
+    create_html = html => {
         const div= document.createElement('div');
         div.innerHTML= html;
         return div.children[0];
+    };
+
+    const destroy_splash = () => {
+        start_button.removeEventListener('click', destroy_splash);
+        splash_element.remove();
+        game_screen();
     }
 
     const build_splash = () => {
@@ -29,6 +34,20 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         start_button= document.getElementById('start_button');
         start_button.addEventListener('click', destroy_splash);
+
+    }
+
+    const game_screen = () => {
+        canvas_element= create_html(`<canvas id="canvas" width= "1350" height= "730"></canvas>`);
+        main_content_element.appendChild(canvas_element);
+        let canvas= document.getElementById('canvas'),
+        ctx= canvas.getContext('2d');
+
+        game= new Game({
+            ctx: ctx,
+            canvas: canvas,
+            player: new Player(canvas, width, height, callback) 
+        })
 
     }
 
