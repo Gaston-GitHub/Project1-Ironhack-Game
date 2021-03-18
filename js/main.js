4c725db7b04dbd4fa8d5bedf4bf70a89d4682fc3
Gaston <gastonsalimbene@gmail.com>
2021-03-18 13:09:16 +0100

    landing screen done

--- js/main.js
+++ js/main.js
@@ -1,99 +1,41 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    
+    const main_content_element = document.getElementById('content');
 
+    let name,
+        game,
 
+        start_button,
+        restart_button,
 
-document.addEventListener('DOMContentLoaded', () => {
-        
-    let gameWindowElement= document.querySelector('#game-window');
-    let game;
+        splash_element,
+        canvas_element,
+        end_game_element;
+
+    window.create_html= html => {
+        const div= document.createElement('div');
+        div.innerHTML= html;
+        return div.children[0];
+    }
+
+    const build_splash = () => {
+        splash_element = create_html(`<div id="splash_container">
+        <h1>Title</h1>
+        <p>Instructions</p>
+        <button id="start_button">Start Game</button>
+        </div>`);
+
+        main_content_element.appendChild(splash_element);
+
+        start_button= document.getElementById('start_button');
+        start_button.addEventListener('click', destroy_splash);
+
+    }
+
+    build_splash();
     
 
-    // ------- landing page ------
-
-    let landingElement;
-    let startGameBtn;
-    let clickStart= function() {
-         leaveLandingPage();
-         
-    }
-
-    function landingPage() {
-
-        landingElement= document.createElement('div');
-        landingElement.setAttribute('id', 'landing');
-        
-        const title= document.createElement('h1');
-        title.innerText= 'homer';
-        landingElement.appendChild(title);
-        const description= document.createElement('h3')
-        description.innerText= 'la taberna de Moe';
-        landingElement.appendChild(description);
-
-        startGameBtn= document.createElement('button');
-        startGameBtn.innerText= 'Start Game';
-        landingElement.appendChild(startGameBtn);
-
-        gameWindowElement.appendChild(landingElement);
-
-        startGameBtn.addEventListener('click', clickStart )
-        
-    }
-
-    function leaveLandingPage() {
-        startGameBtn.removeEventListener('click', clickStart);
-        landingElement.remove();
-    }
-
-    // ------- game page -------
 
 
-    function moveToGame() {
-        
-    }
-    
-
-    // ------- game over page ------
-
-    let gameOverElement;
-    let playAgainBtn;
-    let clickToRestart= function() {
-        leaveGameOver();
-        moveToGame();
-
-    }
-    
-    function moveToGameOver() {
-
-        gameOverElement= document.createElement('div');
-        gameOverElement.setAttribute('id', 'game-over');
-
-        const title= document.createElement('h5');
-        title.innerText= 'Get drunk, Homer!';
-        gameOverElement.appendChild(title);
-
-        const yourScore= document.createElement('h4');
-        yourScore.innerText= 'SCORE: ' + game.score;
-        gameOverElement.appendChild(yourScore);
-
-        playAgainBtn= document.createElement('button');
-
-        playAgainBtn.setAttribute('id', 'play-again');
-        playAgainBtn.innerHTML= 'Play again!';
-        gameOverElement.appendChild(playAgainBtn);
-
-        gameWindowElement.appendChild(gameOverElement);
-
-        playAgainBtn.addEventListener('click', clickToRestart)
-
-    }
-
-    function leaveGameOver() {
-        playAgainBtn.removeEventListener('click', clickToRestart);
-        gameOverElement.remove();
-
-    }
-
-    landingPage();
-    
    
-});
-
+})
